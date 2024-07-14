112d5261d0d46be26b461b6973367e77752ff8c7
Melissa Cutts <admin@thecodingphoenix.com>
2024-07-13 21:39:57 -0500

    Updates

---
+++ precache-manifest.7c71c17748e41a0734e8790fbf13c851.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aebccadbf3e1392485499b40ad5a1ef",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "712a8f83a4ad361be010",
+    "url": "/RobotFriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "b43d1e8e22bfa68e8a7a",
+    "url": "/RobotFriends/static/css/main.2b30f380.chunk.css"
+  },
+  {
+    "revision": "712a8f83a4ad361be010",
+    "url": "/RobotFriends/static/js/2.5d559036.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/RobotFriends/static/js/2.5d559036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c841e7ab423e97409aef",
+    "url": "/RobotFriends/static/js/3.03683dd2.chunk.js"
+  },
+  {
+    "revision": "b43d1e8e22bfa68e8a7a",
+    "url": "/RobotFriends/static/js/main.a1b5c39a.chunk.js"
+  },
+  {
+    "revision": "76a5ae410886921515c7",
+    "url": "/RobotFriends/static/js/runtime-main.60c92145.js"
+  }
+]);
